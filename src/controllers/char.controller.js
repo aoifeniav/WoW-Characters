0a39f0d03d4ec847a556a06c489ed85e504ac3dc
Edén <claudio.ruiz@bootcamp-upgrade.com>
2021-08-12 19:52:47 +0200

    FEAT: creating/editing a character with or without guild now properly creates/edits said guild

--- src/controllers/char.controller.js
+++ src/controllers/char.controller.js
@@ -15,7 +15,6 @@ const newCharGet = async (req, res, next) => {
     return res.status(200).render('./chars/new-char', { user: req.user });
 };
 
-// TODO: Crear relación guild-char al crear y editar personaje.
 const newCharPost = async (req, res, next) => {
     try {
         let { name, faction, race, charClass, level, realm, guild, pic, owner, ...professions } = req.body;
@@ -24,46 +23,40 @@ const newCharPost = async (req, res, next) => {
             professions[profession] = professions[profession] === 'on' ? true : false;
         }
 
-        const existingGuild = await Guild.findOne({ name: guild });
-        if (existingGuild) {
-            guild = existingGuild._id;
-        } else {
-            const guildData = { name: guild, faction, realm }
-            const newGuild = new Guild(guildData);
-            await newGuild.save();
-            guild = newGuild._id;
+        let guildDoc;
+        if (guild.trim() !== '') {
+            guildDoc = await Guild.findOne({ name: guild });
+            if (guildDoc === null) {
+                const guildData = { name: guild, faction, realm }
+                guildDoc = new Guild(guildData);
+                await guildDoc.save();
+            }
         }
 
-        const characterData = { name, faction, race, charClass, level, realm, guild, pic: req.picUrl, owner, professions }
+        const characterData = { name, faction, race, charClass, level, realm, pic: req.picUrl, owner, professions };
+
+        if (guildDoc) {
+            characterData.guild = guildDoc._id;
+        }
+
         const newChar = new Char(characterData);
-        const createdChar = await newChar.save();
+        await newChar.save();
 
         await User.findByIdAndUpdate(
             req.user._id,
-            { $addToSet: { chars: createdChar._id } },
+            { $addToSet: { chars: newChar._id } },
             { new: true }
         );
 
-        // Cuando la guild ya existe y se crea un personaje nuevo con el mismo nombre de guild, se crea una nueva guild con nombre=id de la guild buena por la línea 55 (name: guild).
-        if (existingGuild) {
+        if (guildDoc) {
             await Guild.findByIdAndUpdate(
-                existingGuild._id,
-                { $addToSet: { members: createdChar._id } },
-                { new: true }
-            );
-        } else {
-            const guildData = { name: guild, faction, realm }
-            const newGuild = new Guild(guildData);
-            await newGuild.save();
-
-            await Guild.findByIdAndUpdate(
-                newGuild._id,
-                { $addToSet: { members: createdChar._id } },
+                guildDoc._id,
+                { $addToSet: { members: newChar._id } },
                 { new: true }
             );
         }
 
-        return res.redirect(`/chars/${createdChar._id}`);
+        return res.redirect(`/chars/${newChar._id}`);
     } catch (error) {
         return next(error);
     }
@@ -72,8 +65,9 @@ const newCharPost = async (req, res, next) => {
 const editCharGet = async (req, res, next) => {
     try {
         const char = await Char.findById(req.params.id);
+        const guild = await Guild.findById(char.guild);
 
-        return res.render('./chars/edit-char', { char });
+        return res.render('./chars/edit-char', { char, guild });
     } catch (error) {
         return next(error);
     }
@@ -82,23 +76,52 @@ const editCharGet = async (req, res, next) => {
 const editCharPut = async (req, res, next) => {
     const { id } = req.params;
     try {
-        const { name, faction, race, charClass, level, realm, pic, ...professions } = req.body;
+        const { name, faction, race, charClass, level, realm, guild, pic, ...professions } = req.body;
 
         for (let profession in professions) {
             professions[profession] = professions[profession] === 'on' ? true : false;
         }
 
-        const characterData = { name, faction, race, charClass, level, realm, professions }
+        const characterDoc = await Char.findById(id);
+
+        let guildDoc = await Guild.findOne({ name: guild });
+        if (guildDoc === null) {
+            const guildData = { name: guild, faction, realm }
+            guildDoc = new Guild(guildData);
+            await guildDoc.save();
+
+            let oldGuildDoc = await Guild.findOne({ members: id });
+            if (oldGuildDoc) {
+                const guildMemberIndex = oldGuildDoc.members.findIndex((memberId) => memberId == id);
+                if (guildMemberIndex !== -1) {
+                    oldGuildDoc.members.splice(guildMemberIndex, 1);
+                    oldGuildDoc.save();
+                }
+
+                if (oldGuildDoc.members.length === 0) {
+                    await Guild.findByIdAndDelete(oldGuildDoc._id);
+                }
+            }
+        }
+
+        const characterData = { name, faction, race, charClass, level, realm, guild: guildDoc._id, professions }
 
         if (req.picUrl) {
             characterData.pic = req.picUrl;
         }
 
-        await Char.findOneAndUpdate(
+        const editedChar = await Char.findOneAndUpdate(
             { _id: id },
             characterData,
             { new: true }
         );
+
+        await Guild.findByIdAndUpdate(
+            guildDoc._id,
+            { $addToSet: { members: editedChar._id } },
+            { new: true }
+        );
+
         return res.redirect(`/chars/${id}`);
     } catch (error) {
         return next(error);
@@ -110,6 +133,8 @@ const charGet = async (req, res, next) => {
 
     try {
         const char = await Char.findById(id);
+        const guild = await Guild.findById(char.guild);
+
         const professions = [];
         for (let profession in char.toObject().professions) {
             if (char.toObject().professions[profession]) {
@@ -117,7 +142,7 @@ const charGet = async (req, res, next) => {
             }
         };
 
-        return res.status(200).render('./chars/char', { char, professions });
+        return res.status(200).render('./chars/char', { char, professions, guild });
     } catch (error) {
         next(error);
     };
@@ -144,7 +169,7 @@ const charDelete = async (req, res, next) => {
         const deletedCharGuild = await Guild.findById(deleted.guild);
 
         if (deletedCharGuild.members.length === 0) {
-           await Guild.findByIdAndDelete(deletedCharGuild._id);
+            await Guild.findByIdAndDelete(deletedCharGuild._id);
         }
 
         if (!deleted) {
--- src/models/Guild.model.js
+++ src/models/Guild.model.js
@@ -8,7 +8,6 @@ const guildSchema = new Schema(
         faction: { type: String, required: true },
         realm: { type: String, required: true },
         founded: { type: Number },
-        master: { type: mongoose.Types.ObjectId, ref: 'Guilds' },
         members: [ { type: mongoose.Types.ObjectId, ref: 'Characters' } ]
     },
     { timestamps: true }
